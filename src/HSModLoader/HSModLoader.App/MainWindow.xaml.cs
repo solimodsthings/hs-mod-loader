202a7580ca124b7c2fa08f547eb320202228c707
solimodsthings <85382851+solimodsthings@users.noreply.github.com>
2021-08-07 14:34:40 -0600

    Added new main window design per prototype

--- src/HSModLoader/HSModLoader.App/MainWindow.xaml.cs
+++ src/HSModLoader/HSModLoader.App/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HSModLoader;
 
 namespace HSModLoader.App
 {
@@ -20,9 +21,67 @@ namespace HSModLoader.App
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<ManagedMod> Mods;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Mods = new List<ManagedMod>();
+            this.ListAvailableMods.ItemsSource = Mods;
+
+            // Special case for Superwolf mod which is part of base game install
+            // Moves this to mods.json later
+            var superwolf = new ManagedMod()
+            {
+                Name = "SuperWolf",
+                Version = "1.0",
+                Author = "Nathaniel3W",
+                HasMutator = true,
+                MutatorStartClass = "rpgtacgame.RPGTacMutator_SuperWolf"
+            };
+
+            this.Mods.Add(superwolf);
+            this.ListAvailableMods.SelectedIndex = 0;
+
+            UpdateOrderValue();
+            UpdateInfoBox();
+
+        }
+
+
+        private void UpdateOrderValue()
+        {
+            int order = 1;
+            foreach(var mod in Mods)
+            {
+                mod.Order = order++;
+            }
+        }
+
+        private void UpdateInfoBox()
+        {
+            int selection = this.ListAvailableMods.SelectedIndex;
+
+            if(selection >= 0)
+            {
+                var mod = this.Mods[selection];
+                this.LabelModName.Content = mod.Name;
+                this.LabelModVersion.Content = mod.Version;
+                this.LabelAuthorName.Content = mod.Author;
+                this.LabelModSourceUrl.Content = mod.OptionalUrl;
+
+                // TODO: use binding instead
+                this.RadioButtonIsDisabled.IsChecked = (mod.State == ModState.Disabled);
+                this.RadioButtonIsEnabled.IsChecked = (mod.State == ModState.Enabled);
+                this.RadioButtonIsSoftDisabled.IsChecked = (mod.State == ModState.SoftDisabled);
+
+            }
+        }
+
+        private void OnSelectedModChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.UpdateInfoBox();
         }
     }
 }
---
+++ src/HSModLoader/HSModLoader.App/ManagedMod.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSModLoader.App
+{
+    public enum ModState
+    {
+        Disabled,
+        SoftDisabled,
+        Enabled
+    }
+
+    public class ManagedMod : Mod
+    {
+        public ModState State { get; set; }
+
+        public int Order { get; set; }
+
+        public string TruncatedName { 
+            get
+            {
+                if(this.Name.Length <= 32)
+                {
+                    return this.Name;
+                }
+                else
+                {
+                    return this.Name.Substring(0, 29) + "...";
+                }    
+            } 
+        }
+
+        public string OptionalUrl
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(this.Url) && !string.IsNullOrWhiteSpace(this.Url))
+                {
+                    return this.Url;
+                }
+                else
+                {
+                    return "N/A";
+                }
+            }
+        }
+
+        public ManagedMod()
+        {
+            State = ModState.Disabled;
+        }
+
+    }
+}
