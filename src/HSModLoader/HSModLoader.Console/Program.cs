4b4474c017621f9c1731abeb623aac0653f0c6c3
solimodsthings <85382851+solimodsthings@users.noreply.github.com>
2021-08-08 22:54:00 -0600

    Added small project for quickly generating json files

---
+++ src/HSModLoader/HSModLoader.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+
+namespace HSModLoader.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mod = new Mod()
+            {
+                Name = "Test Mod",
+                Version = "0.9.9",
+                Author = "Test Author",
+                HasCampaign = false,
+                HasMutator = true,
+                MutatorStartClass = "Nonexistent.MutatorClass"
+            };
+
+            var options = new JsonSerializerOptions();
+            options.WriteIndented = true;
+
+            var output = JsonSerializer.Serialize(mod, options);
+
+            System.Console.WriteLine(output);
+            System.Console.ReadLine();
+
+        }
+    }
+}
