5445db9abc8165f8f1c1aee64866b1ca8bc4edc8
solimodsthings <85382851+solimodsthings@users.noreply.github.com>
2021-08-10 00:54:28 -0600

    ConfigurableMod is now ModConfiguration and does not inherit from Mod

---
+++ src/HSModLoader/HSModLoader.App/ModConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSModLoader.App
+{
+    public enum ModState
+    {
+        Disabled,
+        SoftDisabled,
+        Enabled
+    }
+
+    public class ModConfiguration
+    {
+
+        [JsonIgnore]
+        public Mod Mod { get; set; }
+
+        /// <summary>
+        /// The location of unpackaged mod files that will be
+        /// applied to the game folder if this mod is enabled.
+        /// This is also the location of the mod.json file for
+        /// this mod.
+        /// </summary>
+        public string Path { get; set; }
+
+        public ModState State { get; set; }
+
+        public int OrderIndex { get; set; }
+
+    }
+}
