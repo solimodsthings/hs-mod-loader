b12163086f262618d451d0198f715373cb209e36
solimodsthings <85382851+solimodsthings@users.noreply.github.com>
2021-08-12 02:08:11 -0600

    Console program now creates more than one type of mod package for testing

--- src/HSModLoader/HSModLoader.Console/Program.cs
+++ src/HSModLoader/HSModLoader.Console/Program.cs
@@ -1,30 +1,88 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 using System.Text.Json;
 
 namespace HSModLoader.Console
 {
     public class Program
     {
+        private const string ModInfoFile = "mod/mod.json";
+        private const string ModPackageExtension = ".hsmod";
+        private const string ModWorkingFolder = "mod";
+
         public static void Main(string[] args)
+        {
+
+            if (!Directory.Exists(ModWorkingFolder))
+            {
+                Directory.CreateDirectory(ModWorkingFolder);
+            }
+
+            var options = new JsonSerializerOptions() { WriteIndented = true };
+
+            var mods = new Mod[]
+            {
+                InstantiateModWithNoFiles(),
+                InstantiateBrokenLocalizationMod()
+            };
+
+            foreach (var mod in mods)
+            {
+                var output = JsonSerializer.Serialize(mod, options);
+                File.WriteAllText(ModInfoFile, output);
+
+                var filename = mod.Name.Replace(" ", string.Empty).Trim() + ModPackageExtension;
+
+                if (File.Exists(filename))
+                {
+                    File.Delete(filename);
+                }
+
+                ZipFile.CreateFromDirectory(ModWorkingFolder, filename);
+
+                System.Console.WriteLine(string.Format("Created test mod '{0}'", filename));
+
+            }
+
+
+        }
+
+        private static Mod InstantiateModWithNoFiles()
         {
             var mod = new Mod()
             {
-                Name = "Test Mod",
-                Version = "0.9.9",
+                Name = "No Files Test Mod",
+                Version = "0.9.1",
                 Author = "Test Author",
                 HasCampaign = false,
                 HasMutator = true,
                 MutatorStartClass = "Nonexistent.MutatorClass"
             };
 
-            var options = new JsonSerializerOptions();
-            options.WriteIndented = true;
+            mod.AddScript("test.u");
+            mod.AddLocalization("some-localization-file.INT");
 
-            var output = JsonSerializer.Serialize(mod, options);
-
-            System.Console.WriteLine(output);
-            System.Console.ReadLine();
-
+            return mod;
         }
+        private static Mod InstantiateBrokenLocalizationMod()
+        {
+            var mod = new Mod()
+            {
+                Name = "Broken Localization Mod",
+                Version = "0.9.1",
+                Author = "Test Author",
+                HasCampaign = false,
+                HasMutator = true,
+                MutatorStartClass = "Nonexistent.MutatorClass"
+            };
+
+            mod.AddScript("test.u");
+            mod.AddLocalization("some-localization-file.INT");
+            mod.AddLocalization("non-existent-language.XYZ");
+
+            return mod;
+        }
+
     }
 }
