0f9ffa66e17b36f249fe5d0fe280139c3da92586
solimodsthings <85382851+solimodsthings@users.noreply.github.com>
2021-08-21 21:49:16 -0600

    Reduced LogFileExtensions into ErrorLogExtensions class

--- src/HSModLoader/HSModLoader/ErrorLogExtensions.cs
+++ src/HSModLoader/HSModLoader/ErrorLogExtensions.cs
@@ -10,18 +10,13 @@ namespace HSModLoader
     /// <summary>
     /// Used to send information to an error log file.
     /// </summary>
-    public static class LogFileExtensions
+    public static class ErrorLogExtensions
     {
         private static readonly string ErrorLog = "error.log";
 
         public static void AppendToLogFile(this Exception e)
         {
             File.AppendAllText(ErrorLog, string.Format("\n\n[{0}]\n{1}\n{2}", DateTime.Now.ToString(), e.Message, e.StackTrace));
-        }
-
-        public static void AppendToLogFile(this string s)
-        {
-            File.AppendAllText(ErrorLog, string.Format("\n\n[{0}]\n{1}", DateTime.Now.ToString(), s));
         }
 
     }
