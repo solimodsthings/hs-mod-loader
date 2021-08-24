8dc0884ef053bc4accd8f22c48a03ecf0a99cc8e
solimodsthings <85382851+solimodsthings@users.noreply.github.com>
2021-08-24 00:31:57 -0600

    Added Steam Workshop support to publishing tool

--- src/HSModLoader/HSModLoader.App.Publishing/MainWindow.xaml.cs
+++ src/HSModLoader/HSModLoader.App.Publishing/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Ookii.Dialogs.WinForms;
+using Steamworks;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
@@ -8,6 +10,7 @@ using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -31,6 +34,9 @@ namespace HSModLoader.App.Publishing
         private ModContext CurrentModContext { get; set; }
 
         private FileSystemWatcher FileWatcher { get; set; }
+
+        private ulong UploadItemId { get; set; }
+        private bool UploadSubmissionComplete { get; set; }
 
         public MainWindow()
         {
@@ -69,7 +75,7 @@ namespace HSModLoader.App.Publishing
             }
         }
 
-        private void Refresh()
+        private void RefreshFiles()
         {
             Dispatcher.Invoke(() =>
             {
@@ -191,7 +197,7 @@ namespace HSModLoader.App.Publishing
             this.CurrentModContext.Mod = mod;
             this.CurrentModContext.Directory = directory;
             this.FileWatcher.Path = directory;
-            this.Refresh();
+            this.RefreshFiles();
         }
 
         private void OnSaveButtonClick(object sender, RoutedEventArgs e)
@@ -203,7 +209,7 @@ namespace HSModLoader.App.Publishing
 
         private void OnFilesChanged(object sender, FileSystemEventArgs e)
         {
-            this.Refresh();
+            this.RefreshFiles();
         }
 
         private void OnOpenDirectoryButtonClick(object sender, RoutedEventArgs e)
@@ -218,7 +224,49 @@ namespace HSModLoader.App.Publishing
         private void OnPublishButtonClick(object sender, RoutedEventArgs e)
         {
 
+            if (this.CurrentModContext.Mod == null)
+            {
+                return;
+            }
+
             this.ShowOverlay(true);
+            var publish = new PublishingWindow() { Owner = this };
+            var result = publish.ShowDialog();
+
+            if (result == true)
+            {
+                
+                var selection = publish.SelectedPublishingMode;
+
+                if(selection == PublishingMode.Standalone)
+                {
+                    this.ShowProgressOverlay(true);
+
+                    this.PublishStandaloneMod();
+
+                    this.ShowProgressMessage(string.Empty);
+                    this.ShowProgressOverlay(false);
+                    this.ShowOverlay(false);
+                }
+                else
+                {
+                    this.ShowProgressOverlay(true);
+                    var background = new BackgroundWorker();
+                    background.DoWork += PublishSteamMod;
+                    background.RunWorkerCompleted += OnPublishSteamModComplete;
+                    background.RunWorkerAsync();
+                }
+            }
+            else
+            {
+                this.ShowProgressMessage(string.Empty);
+                this.ShowProgressOverlay(false);
+                this.ShowOverlay(false);
+            }
+        }
+
+        private void PublishStandaloneMod()
+        {
 
             try
             {
@@ -265,13 +313,167 @@ namespace HSModLoader.App.Publishing
                     ZipFile.CreateFromDirectory(this.CurrentModContext.Directory, path);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 this.ShowPopupMessage("Error", string.Format("Could not publish file: {0}", ex.Message));
             }
 
-            this.ShowOverlay(false);
-
         }
+
+        private void PublishSteamMod(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                var mod = this.CurrentModContext.Mod;
+
+                bool isNewItem = false;
+                this.UploadItemId = 0;
+                this.UploadSubmissionComplete = false;
+
+                if (mod.SteamWorkshopId.HasValue)
+                {
+                    this.UploadItemId = mod.SteamWorkshopId.Value;
+                }
+
+                this.ShowProgressMessage("Accessing Steam");
+
+                if (SteamAPI.Init())
+                {
+                    var appId = new AppId_t((uint)669500);
+
+                    if (UploadItemId == 0)
+                    {
+
+                        this.ShowProgressMessage("Creating new Steam Workshop item");
+
+                        var createItemCall = SteamUGC.CreateItem(appId, EWorkshopFileType.k_EWorkshopFileTypeCommunity);
+                        var createResult = CallResult<CreateItemResult_t>.Create(OnSteamWorkshopItemCreation);
+                        createResult.Set(createItemCall);
+
+                        while (UploadItemId == 0)
+                        {
+                            SteamAPI.RunCallbacks();
+                            Thread.Sleep(1000);
+                        }
+
+                        this.CurrentModContext.Mod.SteamWorkshopId = UploadItemId;
+                        this.CurrentModContext.SteamId = UploadItemId.ToString();
+                        isNewItem = true;
+                    }
+
+                    this.ShowProgressMessage("Updating Steam Workshop item");
+
+                    var updateHandle = SteamUGC.StartItemUpdate(appId, new PublishedFileId_t(UploadItemId));
+
+                    if(isNewItem)
+                    {
+                        SteamUGC.SetItemTitle(updateHandle, mod.Name);
+                        SteamUGC.SetItemDescription(updateHandle, mod.Description);
+                        SteamUGC.SetItemVisibility(updateHandle, ERemoteStoragePublishedFileVisibility.k_ERemoteStoragePublishedFileVisibilityPublic);
+                    }
+                    
+                    SteamUGC.SetItemContent(updateHandle, this.CurrentModContext.Directory);
+
+                    var submitItemCall = SteamUGC.SubmitItemUpdate(updateHandle, "Updated on " + DateTime.Now.ToString());
+                    var submitResult = CallResult<SubmitItemUpdateResult_t>.Create(OnSteamWorkshopItemSubmission);
+                    submitResult.Set(submitItemCall);
+
+                    this.ShowProgressMessage("Uploading mod files");
+
+                    while (!UploadSubmissionComplete)
+                    {
+                        SteamAPI.RunCallbacks();
+
+                        // ulong bytesDone;
+                        // ulong bytesTotal;
+                        // EItemUpdateStatus status = SteamUGC.GetItemUpdateProgress(updateHandle, out bytesDone, out bytesTotal);
+
+                        Thread.Sleep(1000);
+                    }
+
+                    SteamAPI.Shutdown();
+                    this.ShowProgressMessage(string.Empty);
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        this.Save();
+                        this.ShowProgressOverlay(false);
+
+                        if (isNewItem)
+                        {
+                            this.ShowPopupMessage("Success", "Successfully uploaded your mod as a new Steam Workshop item!"
+                                + " Visit your new workshop item in Steam to add screenshots.");
+                        }
+                        else
+                        {
+                            this.ShowPopupMessage("Success", "Successfully updated the Steam Workshop item for this mod!"
+                                + " Visit your workshop item in Steam to edit your change notes.");
+                        }
+                        
+                    });
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    this.ShowPopupMessage("Error", ex.Message);
+                });
+            }
+        }
+
+        private void ShowProgressMessage(string message)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                this.TextboxProgress.Text = message;
+                
+                if (string.IsNullOrEmpty(message))
+                {
+                    this.TextboxProgress.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    this.TextboxProgress.Visibility = Visibility.Visible;
+                }
+            });
+        }
+
+        private void OnPublishSteamModComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                this.ShowProgressOverlay(false);
+                this.ShowOverlay(false);
+            });
+        }
+
+        private void OnSteamWorkshopItemCreation(CreateItemResult_t result, bool failure)
+        {
+            if (result.m_eResult == EResult.k_EResultOK)
+            {
+                this.UploadItemId = ulong.Parse(result.m_nPublishedFileId.ToString());
+            }
+            else
+            {
+                this.ShowPopupMessage("Error", "Failed to create a new Steam Workshop item for your mod.");
+                this.OnPublishSteamModComplete(null, null);
+            }
+        }
+
+        private void OnSteamWorkshopItemSubmission(SubmitItemUpdateResult_t result, bool failure)
+        {
+            if (result.m_eResult == EResult.k_EResultOK)
+            {
+                this.UploadSubmissionComplete = true;
+            }
+            else
+            {
+                this.ShowPopupMessage("Error", "Failed to upload mod files to the Steam Workshop.");
+                this.OnPublishSteamModComplete(null, null);
+            }
+        }
+
     }
 }
