ec9c1999ef5d1143b9a4bf51121fedaba8fa3d9d
solimodsthings <85382851+solimodsthings@users.noreply.github.com>
2021-12-19 21:23:25 -0700

    New class for taking snapshots of mod configuration

---
+++ src/HSModLoader/HSModLoader.App/ModConfigurationSnapshot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSModLoader.App
+{
+    public class ModConfigurationSnapshot
+    {
+        public ModConfiguration Mod { get; set; }
+        public int OrderIndexSnapshot { get; set; }
+        public ModState ModStateSnapshot { get; set; }
+
+        public ModConfigurationSnapshot(ModConfiguration mod)
+        {
+            this.Mod = mod;
+            this.OrderIndexSnapshot = mod.OrderIndex;
+            this.ModStateSnapshot = mod.State;
+        }
+    }
+}
